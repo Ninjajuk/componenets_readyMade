bbafa2adf5770e0273c81a2860cccab7d8405445
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2023-12-24 16:24:57 +0530

    sidebar with dashboard layout

---
+++ app/sidebar/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import BrandSidebarwithheader from './sidebarcomponents/BrandSidebarwithheader'
+import Darksidebarwithheader from './sidebarcomponents/Darksidebarwithheader'
+import Lightsidebarwithheader from './sidebarcomponents/Lightsidebarwithheader'
+import Darksidebar from './sidebarcomponents/Darksidebar'
+import Lightsidebarwithconstrainedcontentarea from './sidebarcomponents/Lightsidebarwithconstrainedcontentarea'
+import Lightsidebar from './sidebarcomponents/Lightsidebar'
+
+const SidebarPage = () => {
+  return (
+    <>
+    {/* <BrandSidebarwithheader/> */}
+    {/* <Darksidebarwithheader/> */}
+    {/* <Lightsidebarwithheader/> */}
+    {/* <Darksidebar/> */}
+    {/* <Lightsidebarwithconstrainedcontentarea/> */}
+    <Lightsidebar/>
+    </>
+    
+  )
+}
+
+export default SidebarPage
