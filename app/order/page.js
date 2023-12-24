96b15ef507739b3f771b31f2e5443bcc8b906111
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2023-12-24 19:37:31 +0530

    orderhistory

---
+++ app/order/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import InvoicewithAction from './order/InvoicewithAction'
+import OrderHistoryInvoicePanel from './order/OrderHistoryPanel';
+
+const OrderHisotyPage = () => {
+  return (
+    <>
+    <InvoicewithAction/>
+    <OrderHistoryInvoicePanel/>
+    </>
+  )
+}
+
+export default OrderHisotyPage;
