522dbc43bb3cbdb56ce6db5b31facf794482e32a
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-01-01 11:49:43 +0530

    deletemodal

--- app/modal/page.js
+++ app/modal/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PaymentSuccessful from './compoModal/PaymentSuccessful'
+import Deactivateaccount from './compoModal/Deactivateaccount'
 
 const Modalpage = () => {
   return (
     <>
     <h1>Modal</h1>
     <PaymentSuccessful/>
+    <Deactivateaccount/>
     </>
   )
 }
