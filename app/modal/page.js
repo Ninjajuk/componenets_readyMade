78b80c71537af7832f32d3853f5a31a0d0939c00
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-01-01 11:45:21 +0530

    payment success modal

---
+++ app/modal/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PaymentSuccessful from './compoModal/PaymentSuccessful'
+
+const Modalpage = () => {
+  return (
+    <>
+    <h1>Modal</h1>
+    <PaymentSuccessful/>
+    </>
+  )
+}
+
+export default Modalpage
