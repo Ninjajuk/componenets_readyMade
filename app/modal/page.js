84f705c0561625e5ae16e4b0ee34574d3281a9a6
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-01-01 11:57:10 +0530

    notification

--- app/modal/page.js
+++ app/modal/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PaymentSuccessful from './compoModal/PaymentSuccessful'
 import Deactivateaccount from './compoModal/Deactivateaccount'
+import NotiificationNoti from './compoModal/NotiificationNoti'
 
 const Modalpage = () => {
   return (
@@ -8,6 +9,7 @@ const Modalpage = () => {
     <h1>Modal</h1>
     <PaymentSuccessful/>
     <Deactivateaccount/>
+    <NotiificationNoti/>
     </>
   )
 }
